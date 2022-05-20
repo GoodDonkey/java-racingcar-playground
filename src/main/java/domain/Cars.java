853adb3eb80124a7916a187c5d61fc9e68feaa4e
GoodDonkey <president20500@gmail.com>
2022-05-20 20:22:30 +0900

    feat: Cars moveAll() and tests

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -1,2 +1,47 @@
-package domain;public class Cars {
+package domain;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Cars implements Iterable<Car> {
+    
+    private List<Car> cars = new ArrayList<>();
+    
+    public Cars(String names) {
+        String[] split = names.split(",");
+        for (int i = 0; i < split.length; i++) {
+            cars.add(new Car(split[i]));
+        }
+    }
+    
+    public int size() {
+        return cars.size();
+    }
+    
+    public CarLocation getCarLocationOf(Integer index) {
+        return cars.get(index).getCarLocation();
+    }
+    
+    public void moveAll() {
+        // car.move() 결과는 모두 함께 적용받아야 하므로
+        // 복사해서 한번에 넣는 방법을 쓴다. (실패 원자성 보장?)
+        // 쓰레드 세이프하지는 않다.
+        ArrayList<Car> carsTemp = new ArrayList<>();
+        
+        for (Car car : cars) {
+            carsTemp.add(new Car(car));
+        }
+        
+        for (Car car : carsTemp) {
+            car.move();
+        }
+        
+        this.cars = carsTemp;
+    }
+    
+    @Override
+    public Iterator<Car> iterator() {
+        return cars.iterator();
+    }
 }
--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -1,4 +1,49 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class CarsTest {
-  
+    
+    @ParameterizedTest
+    @DisplayName("주어진 이름 개수에 따라 Car 를 가지는가")
+    @CsvSource(value = {"donk,annie,tutu:3", "donk,annie,tutu,qq,ww,ee,rr:7"}, delimiter = ':')
+    void test1(String names, int number) {
+        Cars cars = new Cars(names);
+        assertThat(cars.size()).isEqualTo(number);
+    }
+    
+    @ParameterizedTest
+    @DisplayName("자동차들의 상태를 한번에 바꿀 수 있는가?")
+    @CsvSource(value = "donk,annie,tutu:3", delimiter = ':')
+    void test2(String names, int number) {
+        Cars cars = new Cars(names);
+        CarLocation[] prevLocations = new CarLocation[cars.size()];
+        for (int i = 0; i < cars.size(); i++) {
+            prevLocations[i] = cars.getCarLocationOf(i);
+        }
+        cars.moveAll();
+        // 바뀔 수도 있고 안바뀔 수도 있는데 확률적으로 테스트해도 되나?
+        CarLocation[] postLocations = new CarLocation[cars.size()];
+        for (int i = 0; i < cars.size(); i++) {
+            postLocations[i] = cars.getCarLocationOf(i);
+        }
+        System.out.println(Arrays.toString(prevLocations));
+        System.out.println(Arrays.toString(postLocations));
+        
+        for (int i = 0; i < cars.size(); i++) {
+            System.out.println("prevLocations = " + prevLocations[i]);
+            System.out.println("postLocations[i] = " + postLocations[i]);
+        }
+        // assert 코드 보류
+    }
+
 }
